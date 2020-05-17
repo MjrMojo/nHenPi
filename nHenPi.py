a951d154ffd0cab7f4d31397c928233bf549d00d
MjrMojo <majormojomachine@gmail.com>
2020-05-17 17:43:13 +1200

    Support output a dataset format for nHenPi visualiser
    This inclused formatting titles back to tile format, extracting media
    key element (to display thumbnails), and exporting occurrence location
    in dataset.
    
    Visualiser: display thumbnail on mouseover

--- nHenPi.py
+++ nHenPi.py
@@ -17,6 +17,7 @@ Results as of 2020-05-11 @ 20:23 GMT+13:
 import requests
 import json
 import time
+import re as ree
 
 API_URL_NHENTAI = 'https://nhentai.net/api/gallery/'
 REMOVED = "removed"
@@ -37,6 +38,10 @@ Language split: - {lang1_str:13}: {lang1_num:6} ({lang1_perc:>5.2f} %)
                 - Uncategorised: {uncat_num:6} ({uncat_perc:>5.2f} %)
 """
 
+VISUALISER_OUTPUT = """{{'title': '{}', 'media_id': '{}',
+ 'tags': {}, 'location': [{}, {}],
+ 'language': '{}', 'key': {}}},\n"""
+
 pi1000 = "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989"
 
 #PI = pi1000
@@ -45,6 +50,7 @@ file = open("PI100KDP.TXT")
 PI = "3" + file.readline()
 file.close()
 
+title_extract_regex = ree.compile(r'\([^()]*\)|\[[^[]*\]|\{[^{]*\}')
 
 def count(l, key):
     return sum(map(lambda x: x[2] == key, l))
@@ -68,11 +74,14 @@ def get_data_on_this_filth(key):
     key_data = request.json();
     title_dict = key_data["title"]
     tag_array = key_data["tags"]
+    media_key = key_data["media_id"]
 
     title = title_dict.get("english")
     if title is None:
         #English title doesn't exist
         title = title_dict.get("japanese")
+
+    title = ree.escape(title_extract_regex.sub("", title).strip())
 
     tags = []
     language = ""
@@ -82,7 +91,7 @@ def get_data_on_this_filth(key):
         elif tag["type"] == "language":
             language = tag['name']
 
-    return title, language, tags
+    return title, language, tags, media_key
 
 
 latest_key, latest_key_time_stamp = get_latest_id();
@@ -94,10 +103,10 @@ for i in range(len(PI)-5):
     if key <= latest_key:
         if not key in keys: #ensure the key is unique to prevent duplicates
             keys.add(key)
-            title, language, tags = get_data_on_this_filth(key)
+            title, language, tags, media_key = get_data_on_this_filth(key)
             if title != REMOVED:
                 print("{} [{}, {}]: {}".format(title, key, language, tags))
-                sources.append([key, title, language, tags])
+                sources.append([key, title, language, tags, media_key, (i, i + 6)])
 
 print("\n\n\n\n\nThere are {} weeb numbers in the first {} digits of Pi".format(len(sources), len(PI)))
 
@@ -136,4 +145,16 @@ output_file.write(OUTPUT_HEADER_FORMAT.format(num=num,
 for source in sources:
     output_file.write(OUTPUT_FORMAT_STRING.format(source[0], source[1],
                                                   source[2], source[3]))
+output_file.close()
+
+
+output_file = open("visualiser_dataset.js", "w", encoding='utf-8')
+
+output_file.write("dataset = [\n")
+for source in sources:
+    output_file.write(VISUALISER_OUTPUT.format(source[1], source[4],
+                                               source[3], source[5][0],
+                                               source[5][1], source[2],
+                                               source[0]))
+output_file.write("]")
 output_file.close()
