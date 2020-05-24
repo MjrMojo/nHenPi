4d306c1f9c0b0029ffb21323e3ffa9c53cb62f73
MjrMojo <majormojomachine@gmail.com>
2020-05-24 15:23:24 +1200

    CoverFetcher: increase pixelisation so larger images are censored

--- nHenPiVisualiser/cover_fetcher/pixelate.sh
+++ nHenPiVisualiser/cover_fetcher/pixelate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-convert -scale 5% -scale 2000% -gravity Center -pointsize 75 -fill black -draw 'text 0,0"Loli Porn"' $1  $2
+convert -scale 1% -scale 10000% -gravity Center -pointsize 75 -fill black -draw 'text 0,0"Loli Porn"' $1  $2
