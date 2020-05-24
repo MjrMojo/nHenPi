0b2a1cdc019433794eabf5eed2a50b7545294879
MjrMojo <majormojomachine@gmail.com>
2020-05-24 12:52:26 +1200

    CoverFetcher: attempt to get .png images as a backup to .jpg
    Also changes logging to only display total time taken

--- nHenPiVisualiser/cover_fetcher/testing_flask.py
+++ nHenPiVisualiser/cover_fetcher/testing_flask.py
@@ -10,8 +10,6 @@ app = Flask(__name__)
 
 CORS(app)
 
-LOG = "Request.get: {:.3f} ms\nWriting Cover: {:.3f} ms\nPixelation: {:.3f} ms"
-
 if __name__ == '__main__':
     app.run(threaded=True)
 
@@ -24,21 +22,25 @@ def calc_time(start, end):
 
 @app.route("/fetch_cover/<media_key>")
 def get_cover_from_nhentai(media_key):
+    start_time = time.time()
     url = NHENTAI_COVER_GALLERY.format(media_key, "jpg")
-    req_start_time = time.time()
     request = requests.get(url, stream = True)
-    req_end_time = time.time()
+    if not request.ok:
+        #if the request fails try to get the .png version
+        url = NHENTAI_COVER_GALLERY.format(media_key, "png")
+        request = requests.get(url, stream = True)
+        if not request.ok:
+            #we couldn't even get the png version, just give up
+            print("Handling request took {:.2f}".format(time.time() - start_time))
+            return "done"
 
-    if request.ok:
-        filename = "{}.jpg".format(media_key)
-        request.raw.decode_content = True
-        with open(filename, 'wb') as f:
-            for section in request:
-                f.write(section)
-        file_write_end_time = time.time()
-        subprocess.call(["./pixelate.sh", filename, filename])
-        pixelate_end_time = time.time()
-        print(LOG.format(calc_time(req_start_time, req_end_time),
-                         calc_time(req_end_time, file_write_end_time),
-                         calc_time(file_write_end_time, pixelate_end_time)))
-        return "done"
+    #if we managed to retrieve one of the versions save it out
+    #if its a png store it as a jpg any way
+    filename = "{}.jpg".format(media_key)
+    request.raw.decode_content = True
+    with open(filename, 'wb') as f:
+        for section in request:
+            f.write(section)
+    subprocess.call(["./pixelate.sh", filename, filename])
+    print("Handling request took {:.2f}".format(time.time() - start_time))
+    return "done"
